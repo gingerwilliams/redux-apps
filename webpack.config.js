0deefb9e88e6d0d19a6dc021d573d5f49fad3bad
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2019-10-08 20:55:19 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./counter.js",
+    output: {
+        filename: "bundle.js",
+        publicPath: "dist/",
+        path: path.resolve(__dirname, "dist")
+    },
+    devServer: {
+        port: 8000,
+        open: true,
+        watchContentBase: true,
+        writeToDisk: true,
+        compress: true,
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(ttf|eot)$/,
+                use: [
+                    {
+                        loader: "file-loader"
+                    },
+                    {
+                        loader: "url-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    }
+};
