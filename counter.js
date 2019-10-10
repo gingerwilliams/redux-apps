0deb83d29e9d5512f03436e084e9f65c1070cc45
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2019-10-10 11:36:40 -0400

    in browser display of state

--- counter.js
+++ counter.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
+
+import { Provider } from "react-redux";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 //action
 const increment = () => {
@@ -15,30 +19,71 @@ const decrement = () => {
     };
 };
 
+const isLoggedIn = () => {
+    return {
+        type: "SIGN_IN"
+    };
+};
+
 //reducer
-const counter = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
     switch (action.type) {
         case "INCREMENT":
             return state + 1;
         case "DECREMENT":
             return state - 1;
+        default:
+            return state;
     }
 };
 
+const isLoggedInReducer = (state = false, action) => {
+    switch (action.type) {
+        case "SIGN_IN":
+            return true;
+        default:
+            return state;
+    }
+};
+
+//process reducer
+const allReducers = combineReducers({
+    counterReducer: counterReducer,
+    isLoggedInReducer: isLoggedInReducer
+});
+
+//devtools
+const reduxDevtools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+
 //store
-let store = createStore(counter);
-
-//dispay store in the console
-store.subscribe(() => console.log(store.getState()));
-
-//dispatch
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+const store = createStore(allReducers, reduxDevtools);
 
 const Hello = () => {
-    return <div>Hello Redux {store.getState()}</div>;
+    //display counter
+    const counter = useSelector(state => state.counterReducer);
+    const login = useSelector(state => state.isLoggedInReducer);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <p>Hello Redux: {login ? "Welcome Back" : `Please Log In`}</p>
+            <p>Counter: {counter}</p>
+            <div>
+                <button onClick={() => dispatch(decrement())}>-</button>
+                <button onClick={() => dispatch(increment())}>+</button>
+            </div>
+        </div>
+    );
 };
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
+    document.querySelector("#root")
+);
 
 ///////////
 
@@ -49,4 +94,3 @@ const Hello = () => {
 //reducer : describes how your action transforms state into the next state, check witch action you did (process your order)
 
 //dispatch :  sends the action to the reducer or store ?
-ReactDOM.render(<Hello />, document.querySelector("#root"));
