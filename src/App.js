9a51a30318a84deff2cceb773913c6ceb0a2607b
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2019-10-10 14:10:59 -0400

    added the remove btn dispatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addTodo } from "./actions/actionCreator";
+import { addTodo, removeTodo } from "./actions/actionCreator";
 
 export const App = () => {
     const todo = useSelector(state => state.todo);
@@ -10,6 +10,7 @@ export const App = () => {
             <p>Testing UI</p>
             <ul>{todo}</ul>
             <button onClick={() => dispatch(addTodo())}>+</button>
+            <button onClick={() => dispatch(removeTodo())}>-</button>
         </div>
     );
 };
