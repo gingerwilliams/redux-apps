1c88784c6d95534ee3688e63b05279ba798318fb
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2019-10-29 12:59:48 -0400

    Removed Normalizer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
-import { boards as defaultBoards } from "../normalized-state";
+// import { boards as defaultBoards } from "../normalized-state";
+import { boards as defaultBoards } from "../default-state.json";
+import { ADD_BOARD } from "../actions/actionCreators";
 
-const boardReducer = (boards = defaultBoards, actions) => {
+const boardReducer = (boards = defaultBoards, action) => {
     console.log(boards);
     return boards;
 };
 
 export const rootReducer = combineReducers({
-    board: boardReducer
+    boards: boardReducer
 });
