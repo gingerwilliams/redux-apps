eadceb253bd48374a185ee5f489e871b1f13f579
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2019-10-30 15:37:07 -0400

    styles and markup tweaks

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Post = ({ post }) => {
-    console.log(post);
+    // console.log(post);
     return (
         <li className="Post">
+            <button>🖊</button>
             <h4>{post.title}</h4>
             <a>{post.body}</a>
+
+            <hr />
+            <textarea placeholder="comment" />
+            {/* add a comment */}
         </li>
     );
 };
